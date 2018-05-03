5c7bf40ecdacd4dc938106b5d716c25b7b0da9de
Catherine <cathey191@hotmail.com>
2018-05-03 13:09:26 +1200

    Linked chickensBreedign

--- dougs/chickensList.js
+++ dougs/chickensList.js
@@ -4,12 +4,22 @@ var jsonRead = false;
 
 function recursivelyCheckAll(data, current){
     var htmlString = '<div><p>'+current+'</p></div>'
-
-    for (parent in data[current]){
-               if (data[current][parent] != null){
-                    htmlString += recursivelyCheckAll(data, data[current][parent]);
+		console.log("current: "+current);
+		console.log(data[current])
+    for (var i=0; i<data.length;i++){
+			var tier = data[i]
+			console.log("Tier: ");
+			console.log(tier);
+				for (parent in data[tier][current]){
+               if (data[tier][current][parent] != null){
+								 console.log("Parent:")
+								 console.log(parent);
+								 console.log(current);
+								 console.log("--------------");
+                    htmlString += recursivelyCheckAll(data,data[tier][current][parent]);
                 };
-            }
+            };
+				};
     return htmlString
 };
 
@@ -17,16 +27,18 @@ $(document).on('keyup', '.search', function(event){
     var text = document.querySelector('.search').value;
 
     if (jsonRead == false){
+			console.log("getting json");
         $.getJSON('file:///C:/Users/Flossy/OneDrive/Pictures/WDWU02/chickens/cats/js/chickenBreeds.json', function(data){
 
             console.log(data);
+						console.log("Running....");
             recursivelyCheckAll(data, text);
             jsonRead = true;
             chickens = data;
         });
 
     } else {
-
+				console.log("normal running...");
         var html = recursivelyCheckAll(chickens, text);
         document.querySelector(".output").innerHTML = html;
     }
